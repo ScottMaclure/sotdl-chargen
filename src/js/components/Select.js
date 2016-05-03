f295778b16f0372017329d13c6e58af958d6459c
Scott Maclure <scott@maclure.com.au>
2016-05-03 19:41:45 +1000

    FIXME When using value, the select field doesn't change even though the data does :/

--- src/js/components/Select.js
+++ src/js/components/Select.js
@@ -1,11 +1,12 @@
 import React, { PropTypes } from 'react'
 
 const Select = ({ id, options, value, onChange }) => (
-		<select id={id} name={id} value={value} className="field" onChange={onChange}>
+		<select id={id} name={id} className="field" onChange={onChange}>
 			{options.map(function (option, i) {
 				// Handle structured items or just arrays of strings.
 				let optionValue = option.value || option
-				return <option key={i} value={optionValue}>{optionValue}</option>
+				let isSelected = value === optionValue
+				return <option key={i} value={optionValue} selected={isSelected}>{optionValue}</option>
 			})}
 		</select>
 
