60c04cc3ae1d92cd3f9d13de676fb17793c61241
Scott Maclure <scott@maclure.com.au>
2016-05-05 18:18:25 +1000

    Linting.

--- src/js/reducers/charGen.js
+++ src/js/reducers/charGen.js
@@ -20,8 +20,8 @@ import appData from '../appData.js'
 import charData from '../../data/char.json'
 
 const initialState = {
-	app: appData,
-	char: charData
+  app: appData,
+  char: charData
 }
 
 /**
@@ -29,26 +29,25 @@ const initialState = {
  * TODO Handle non-singular results (i.e. 20 options for a 1d20 roll).
  */
 const getRandomItem = (array) => {
-	let val = array[Math.floor(Math.random() * array.length)]
-	return val.value || val
+  let val = array[Math.floor(Math.random() * array.length)]
+  return val.value || val
 }
 
 const setAncestryData = (state) => {
+  // Now we know the ancestry, we can work everything else out.
+  let ancestryData = state.app[state.char.ancestry.toLowerCase()]
 
-	// Now we know the ancestry, we can work everything else out.
-	let ancestryData = state.app[state.char.ancestry.toLowerCase()]
+  state.char.name = getRandomItem(ancestryData.commonNames)
+  state.char.background = getRandomItem(ancestryData.background)
 
-	state.char.name = getRandomItem(ancestryData.commonNames)
-	state.char.background = getRandomItem(ancestryData.background)
-
-	state.char.attributes = Object.assign({}, ancestryData.attributes)
+  state.char.attributes = Object.assign({}, ancestryData.attributes)
 }
 
 const initRandomCharacter = (state) => {
-	console.error('FIXME forcing ancestry to Human/Orc during dev.');
-	state.char.ancestry = getRandomItem(['Human', 'Orc'])
-	// state.char.ancestry = getRandomItem(state.app.ancestries)
-	setAncestryData(state)
+  console.error('FIXME forcing ancestry to Human/Orc during dev.');
+  state.char.ancestry = getRandomItem(['Human', 'Orc'])
+  // state.char.ancestry = getRandomItem(state.app.ancestries)
+  setAncestryData(state)
 }
 
 // Init new characters with random values.
@@ -56,28 +55,26 @@ initRandomCharacter(initialState)
 
 // root reducer
 // TODO split this up into smaller parts as I develop the app.
-export default function charGen(state = initialState, action) {
+export default function charGen (state = initialState, action) {
+  let newState = {
+    app: Object.assign({}, state.app),
+    char: Object.assign({}, state.char)
+  }
 
-	let newState = {
-		app: Object.assign({}, state.app),
-		char: Object.assign({}, state.char)
-	}
+  switch (action.type) {
+    case 'CREATE_RANDOM':
+      initRandomCharacter(newState)
+      break
+    case 'CHANGE_ANCESTRY':
+      newState.char.ancestry = action.value
+      setAncestryData(newState)
+      break
+    case 'CHANGE_SIMPLE_VALUE':
+      newState.char[action.name] = action.value
+      break
+  }
 
-	switch (action.type) {
-		case 'CREATE_RANDOM':
-			initRandomCharacter(newState)
-			break;
-		case 'CHANGE_ANCESTRY':
-			newState.char.ancestry = action.value
-			setAncestryData(newState)
-			break;
-		case 'CHANGE_SIMPLE_VALUE':
-			newState.char[action.name] = action.value
-			break;
-	}
+  console.log('newState:', newState)
 
-	console.log('newState:', newState)
-
-	return newState
-
+  return newState
 }
