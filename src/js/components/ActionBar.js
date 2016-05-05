0e164267bed8a3bfdc3d7ba79a3d13e07b9c7191
Scott Maclure <scott@maclure.com.au>
2016-05-05 18:18:16 +1000

    Linting.

--- src/js/components/ActionBar.js
+++ src/js/components/ActionBar.js
@@ -3,20 +3,20 @@ import React from 'react'
 import '../../css/actionBar.css'
 
 const getViewStyle = (mode) => ({
-	display: mode !== 'view' ? 'none' : ''
+  display: mode !== 'view' ? 'none' : ''
 })
 
 const getEditStyle = (mode) => ({
-	display: mode !== 'edit' ? 'none' : ''
+  display: mode !== 'edit' ? 'none' : ''
 })
 
 const ActionBar = ({ mode, onEdit, onView, onCreate }) => (
-	<div className="actionBar">
-		<button className="modeButton" onClick={onEdit} style={getViewStyle(mode)}>View/Edit</button>
-		<button className="modeButton" onClick={onView} style={getEditStyle(mode)}>Edit/View</button>
-		<span className="spacer"></span>
-		<button className="createButton" onClick={onCreate}>New Random</button>
-	</div>
+  <div className='actionBar'>
+    <button className='modeButton' onClick={onEdit} style={getViewStyle(mode)}>View/Edit</button>
+    <button className='modeButton' onClick={onView} style={getEditStyle(mode)}>Edit/View</button>
+    <span className='spacer'></span>
+    <button className='createButton' onClick={onCreate}>New Random</button>
+  </div>
 )
 
 export default ActionBar
