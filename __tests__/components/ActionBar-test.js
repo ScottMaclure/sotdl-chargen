fc85f77ba391cba16f3e56f608bb563ce5d0c98c
Scott Maclure <scott@maclure.com.au>
2016-05-09 22:39:18 +1000

    [#1] Updating test suite.

--- __tests__/components/ActionBar-test.js
+++ __tests__/components/ActionBar-test.js
@@ -7,18 +7,20 @@ import ActionBar from '../../src/js/components/ActionBar.js'
 describe('ActionBar Component', () => {
   const shallowRenderer = ReactTestUtils.createRenderer()
   let instance = null
-  let attributes = {}
-  let viewButton = null
+  let attributes = null
+
+  beforeEach(() => {
+    // For convenience - just change what you need.
+    attributes = {
+      mode: 'view',
+      onEdit: jasmine.createSpy(),
+      onView: jasmine.createSpy(),
+      onCreate: jasmine.createSpy()
+    }
+  })
 
   describe('for view mode', () => {
     beforeEach(() => {
-      attributes = {
-        mode: 'view',
-        onEdit: jasmine.createSpy(),
-        onView: jasmine.createSpy(),
-        onCreate: jasmine.createSpy()
-      }
-
       instance = shallowRenderer.render(
         <ActionBar {...attributes} />
       )
@@ -42,19 +44,103 @@ describe('ActionBar Component', () => {
       expect(attributes.onCreate).not.toHaveBeenCalled()
     })
 
-    describe('when onClick is called on viewButton', () => {
+    describe('for viewButton', () => {
       let viewButton = null
 
       beforeEach(() => {
         viewButton = instance.props.children.find(elem => (
           elem.props.className.includes('viewButton')
         ))
-        viewButton.props.onClick()
       })
 
-      it('triggers onEdit', () => {
-        expect(attributes.onEdit).toHaveBeenCalled()
+      it('is not hidden from the User', () => {
+        expect(viewButton.props.style.display).toEqual('')
       })
+
+      describe('when onClick is triggered', () => {
+        beforeEach(() => {
+          viewButton = instance.props.children.find(elem => (
+            elem.props.className.includes('viewButton')
+          ))
+          viewButton.props.onClick()
+        })
+
+        it('triggers onEdit', () => {
+          expect(attributes.onEdit).toHaveBeenCalled()
+        })
+
+        it('triggers no other event functions', () => {
+          expect(attributes.onView).not.toHaveBeenCalled()
+          expect(attributes.onCreate).not.toHaveBeenCalled()
+        })
+      })
+    })
+
+    describe('when onClick is called on editButton', () => {
+      let editButton = null
+
+      beforeEach(() => {
+        editButton = instance.props.children.find(elem => (
+          elem.props.className.includes('editButton')
+        ))
+        editButton.props.onClick()
+      })
+
+      it('is hidden from the User', () => {
+        expect(editButton.props.style.display).toEqual('none')
+      })
+
+      it('triggers onView', () => {
+        expect(attributes.onView).toHaveBeenCalled()
+      })
+
+      it('triggers no other event functions', () => {
+        expect(attributes.onEdit).not.toHaveBeenCalled()
+        expect(attributes.onCreate).not.toHaveBeenCalled()
+      })
+    })
+
+    describe('when onClick is called on createButton', () => {
+      let createButton = null
+
+      beforeEach(() => {
+        createButton = instance.props.children.find(elem => (
+          elem.props.className.includes('createButton')
+        ))
+        createButton.props.onClick()
+      })
+
+      it('triggers createButton', () => {
+        expect(attributes.onCreate).toHaveBeenCalled()
+      })
+
+      it('triggers no other event functions', () => {
+        expect(attributes.onEdit).not.toHaveBeenCalled()
+        expect(attributes.onView).not.toHaveBeenCalled()
+      })
+    })
+  })
+
+  describe('for edit mode', () => {
+    beforeEach(() => {
+      attributes.mode = 'edit'
+      instance = shallowRenderer.render(
+        <ActionBar {...attributes} />
+      )
+    })
+
+    it('hides the view button', () => {
+      const viewButton = instance.props.children.find(elem => (
+        elem.props.className.includes('viewButton')
+      ))
+      expect(viewButton.props.style.display).toEqual('none')
+    })
+
+    it('shows the edit button', () => {
+      const editButton = instance.props.children.find(elem => (
+        elem.props.className.includes('editButton')
+      ))
+      expect(editButton.props.style.display).toEqual('')
     })
   })
 })
