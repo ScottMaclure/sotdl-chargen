f00b2a793d062f6b5dfd1bb0c1df3014ae5a66be
Scott Maclure <scott@maclure.com.au>
2016-05-30 09:41:27 +1000

    [#15] Separate Sheet and CharacterSheet, add tests for CharacterSheet.

---
+++ __tests__/components/CharacterSheet-test.js
@@ -0,0 +1,66 @@
+jest.unmock('js/components/CharacterSheet')
+jest.unmock('js/utils/findChild')
+// TODO This is annoying - have to unmock nested imports.
+jest.unmock('js/utils/isPopulatedArray')
+
+import React from 'react'
+import ReactTestUtils from 'react-addons-test-utils'
+
+import findChild from 'js/utils/findChild'
+
+import CharacterSheet from 'js/components/CharacterSheet'
+
+describe('for CharacterSheet', () => {
+  const shallowRenderer = ReactTestUtils.createRenderer()
+  let instance = null
+  let attributes = null
+
+  describe('for valid inputs in view mode', () => {
+    beforeEach(() => {
+      attributes = {
+        mode: 'view',
+        appData: {
+          attributes: [
+            'strength'
+          ]
+        },
+        charData: {
+          attributes: {
+            strength: 10
+          },
+          characteristics: {
+            'damage': 0
+          }
+        }
+      }
+
+      instance = shallowRenderer.render(
+        <CharacterSheet {...attributes} />
+      )
+    })
+
+    it('renders the component', () => {
+      expect(instance.props.className).toEqual('characterSheet')
+      expect(instance.type).toEqual('div')
+    })
+
+    it('renders visibly', () => {
+      expect(instance.props.style.display).toEqual('none')
+    })
+
+    it('renders a header', () => {
+      const component = findChild(instance, 'type', 'h2')
+      expect(component.props.children).toEqual('Character Sheet')
+    })
+
+    it('renders the info component', () => {
+      const component = findChild(instance, 'props.legend', 'Info')
+      expect(component).toBeDefined()
+    })
+
+    it('renders the attributes component', () => {
+      const component = findChild(instance, 'props.legend', 'Attributes')
+      expect(component).toBeDefined()
+    })
+  })
+})
